d9ffa1e0216d8436b39d0c6b88133cb6525aabd3
Macbook <macbook@Macbooks-MacBook-Pro.local>
2026-02-13 22:40:05 +0700

    fix css

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ onload = () => {
 document.querySelector(".love-btn").onclick = () => {
   alert("I love you ❤️");
 };
+function setRealHeight(){
+  document.documentElement.style.setProperty(
+    "--real100vh",
+    `${window.innerHeight}px`
+  );
+}
+
+setRealHeight();
+window.addEventListener("resize", setRealHeight);
+window.addEventListener("orientationchange", setRealHeight);
